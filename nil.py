8730819c7fb23a0130db5fbda01568c1e3d481c5
MininDiamond123 <34421582+MininDiamond123@users.noreply.github.com>
2018-01-14 12:05:53 -0500

    Update nil.py

--- nil.py
+++ nil.py
@@ -4,4 +4,23 @@ import gui
 
 bot = commands.Bot(command_prefix = "ch.", description = "This is a customized bot for The Clubhouse! If you have any suggestions, please tag a Bot Developer.", owner_id = 389472478545575937)
 
+@bot.command()
+async def flip(ctx):
+    em = discord.Embed(color=discord.Color(value=0xb2acef))
+    em.title = "Coinflip Results:"
+    list = []
+    for heads in range(0, 49):
+        list.append("It landed on Heads.")
+    for tails in range(0, 49):
+        list.append("It landed on Tails.")
+    for side in range(0, 2):
+        list.append("It landed on the side!")
+    flipped = random.choice(list)
+    em.description = f"{flippeds}"
+    await ctx.send(embed=em)
+
+
+
+
+
 bot.run(os.environ.get("TOKEN"))
